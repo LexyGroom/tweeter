fad8fd5f4aa706cb76a12ce836ab2697dccb1dd1
Alexys Groom <lexygroom@gmail.com>
2023-09-22 19:21:05 -0400

    Add GET request to fetch tweets, remove hard-coded tweets object, add timeago script, add time passed using timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense, donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const renderTweets = function(tweets) {
     const $tweetsContainer = $('#tweets-container');
 
@@ -48,7 +23,7 @@ $(document).ready(function() {
         </header>
         <p>${tweet.content.text}</p>
         <footer class="tweets-footer">
-          <p>${tweet.created_at}</p>
+          <p class="timestamp" data-time=${tweet.created_at}></p>
           <div class="icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -60,18 +35,24 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  renderTweets(data);
-});
-
-$(document).ready(function() {
   $("#myForm").submit(function(event) {
     event.preventDefault();
     let formData = $(this).serialize();
 
-    console.log("Serialized Form Data:", formData);
-
     $.post("http://localhost:8080/tweets/", formData, function(response) {
       $(".result").html(response);
+      loadTweets();
     });
   });
+
+  let loadTweets = function() {
+    $.get("http://localhost:8080/tweets", function(data) {
+      renderTweets(data);
+      $('.timestamp').each(function() {
+        const timestamp = $(this).data('time');
+        $(this).text(timeago.format(timestamp));
+      });
+    })
+  }
+  loadTweets();
 });
